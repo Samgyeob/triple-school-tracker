fa750e38adf873850faa9a505c9d101cf02201d2
Anna <05xunc@gmail.com>
2026-05-23 21:09:59 -0500

    add admin page

--- vibe coding projects/Whale extension/background.js
+++
@@ -1,7 +0,0 @@
-// background.js
-// Listen for tab updates and send current tab info to the sidebar
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete") {
-    chrome.runtime.sendMessage({ type: "TAB_UPDATED", title: tab.title, url: tab.url });
-  }
-});
